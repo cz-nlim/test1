4de7fe8cf0620b31f00977a8756c8cd8c46fce5d
cz <chenzhuo12345@sjtu.edu,cn>
2025-10-03 09:28:31 +0800

    Feat: Add Receive and transmit

--- Core/Src/callback01.cpp
+++ Core/Src/callback01.cpp
@@ -1,13 +1,13 @@
 #include <cstring>
 #include "main.h"
 #include "usart.h"
-extern uint8_t rx_msg[4];
-uint8_t buffer[4];
+extern uint8_t rx_msg[10];
+uint8_t buffer[10];
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart == &huart7) {
-        HAL_UART_Receive_IT(&huart7, rx_msg, 3);
-        memcpy(buffer, rx_msg, 3);
+        HAL_UART_Receive_IT(&huart7, rx_msg, 10);
+        memcpy(buffer, rx_msg, 10);
         HAL_UART_Transmit_IT(&huart7, buffer, 10);
     }
 }
